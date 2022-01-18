e3cc9c3d8221340fbe25214ddb5a090a1a0dfa62
337-1 <senyuk@bsuir.by>
2022-01-18 16:16:15 +0300

    update (work)

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,28 @@ import java.util.Scanner;
 public class Main {
     private static int startIndex = 0;
     private static int lastIndex = 0;
+    private static String name;
 
     public static void main(String[] args) {
         char space = ' ';
         Scanner sc = new Scanner(System.in);
-        String intFullName = sc.nextLine().trim();
+        String strFullName;
         do {
             System.out.println("Введите три слова через пробел: \"Фамилия Имя Отчество\"");
-            intFullName = sc.nextLine().trim();
+            strFullName = sc.nextLine().trim();
         }
-        while (checkFullName(intFullName, space));
+        while (checkFullName(strFullName, space));
+        System.out.println(name);
 
     }
 
-    private static boolean checkFullName(String intFullName, char space) {
+    private static boolean checkFullName(String string, char serchPart) {
 
         boolean check = false;
-        startIndex = intFullName.indexOf(space);
-        lastIndex = intFullName.lastIndexOf(space);
-        String name = intFullName.substring(startIndex, lastIndex).trim();
-        int indexCheck = name.indexOf(space);
+        startIndex = string.indexOf(serchPart);
+        lastIndex = string.lastIndexOf(serchPart);
+        name = string.substring(startIndex, lastIndex).trim();
+        int indexCheck = name.indexOf(serchPart);
         if (!(indexCheck == -1)) {
             check = true;
         }
