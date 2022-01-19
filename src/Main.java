1c07a9d19c3068d22d7cb6dcfc4c095db575d5c7
337-1 <senyuk@bsuir.by>
2022-01-19 14:05:24 +0300

    update (work)

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     private static int startIndex = 0;
     private static int lastIndex = 0;
     private static String name;
+//    private static String surName;
+//    private static String otchestvo;
 
     public static void main(String[] args) {
         char space = ' ';
@@ -12,9 +14,8 @@ public class Main {
         do {
             System.out.println("Введите три слова через пробел: \"Фамилия Имя Отчество\"");
             strFullName = sc.nextLine().trim();
-        }
-        while (checkFullName(strFullName, space));
-        System.out.println(outputFullName(strFullName));
+        } while (checkFullName(strFullName, space));
+        System.out.println(getFullName(strFullName));
     }
 
     private static boolean checkFullName(String string, char space) {
@@ -27,17 +28,23 @@ public class Main {
         } else {
             name = string.substring(startIndex, lastIndex).trim();
             int indexCheck = name.indexOf(space);
-            if (!(indexCheck == -1) | name.equals("")) {
-                check = true;
-            }
+            if (!(indexCheck == -1) | name.equals("")) check = true;
         }
         return check;
     }
 
-    private static String outputFullName(String string) {
+    private static String getFullName(String string) {
 
         String surName = string.substring(0, startIndex);
         String otchestvo = string.substring(lastIndex + 1);
         return "Фамилия: " + surName + "\nИмя: " + name + "\nОтчество: " + otchestvo;
     }
+
+   /* private static void setSurName(String string) {
+        surName = string.substring(0, startIndex);
+    }
+
+    private static void setOtchestvo(String string) {
+        otchestvo = string.substring(0, startIndex);
+    }*/
 }
